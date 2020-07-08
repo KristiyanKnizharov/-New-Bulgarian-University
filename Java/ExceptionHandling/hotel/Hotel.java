4e70d8b2f6740b5151dfd478488f592d76bdf657
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-07-09 01:55:13 +0300

    Update Hotel.java

--- Java/ExceptionHandling/hotel/Hotel.java
+++ Java/ExceptionHandling/hotel/Hotel.java
@@ -1,15 +1,8 @@
-package hotel;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package hotel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Kostadinova
- */
 public class Hotel {
     private int numberOfRooms;
     private List<Integer> rooms;
